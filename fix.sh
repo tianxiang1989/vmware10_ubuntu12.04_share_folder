79d796036e378ce27713ef52b7253303eb11deec
tianxiang1989 <qimingbuyi@sina.com>
2015-05-19 22:12:08 +0800

    modify path

--- fix.sh
+++ fix.sh
@@ -1,12 +1,10 @@
 set -x
-cd $home
-cd vmware-tools-distrib/lib/modules/source
+cd $home/Downloads/vmware-tools-distrib/lib/modules/source
 sudo tar xf vmhgfs.tar
 #sudo wget https://raw.github.com/rasa/vmware-tools-patches/master/patches/vmhgfs/vmhgfs-d_count-kernel-3.11-tools-9.6.0.patch
-sudo patch -p0 <vmhgfs-d_count-kernel-3.11-tools-9.6.0.patch
+sudo patch -p0 <$home/Downloads/vmhgfs-d_count-kernel-3.11-tools-9.6.0.patch
 sudo mv vmhgfs.tar vmhgfs.orig.tar
 sudo tar cf vmhgfs.tar vmhgfs-only
-cd $home
-cd vmware-tools-distrib
+cd $home/Downloads/vmware-tools-distrib
 sudo ./vmware-install.pl --clobber-kernel-modules=vmhgfs
 set +x
